265e47ba2577b6c862101f61c1e9f3b44de5c216
fde-capu <flavio@carrara.us>
2024-05-27 12:15:27 -0300

    Added comments on Colleen C.

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,2 +1,9 @@
+/*
+'What is there?' It can be answered, moreover, in a word - 'Everything' - and everyone will accept this answer as true.
+*/
 #include <stdio.h>
-int main(){char*c="\n\t\"\\";char*s="#include <stdio.h>%cint main(){char*c=%c%cn%ct%c%c%c%c%c;char*s=%c%s%c;printf(s,c[0],c[2],c[3],c[3],c[3],c[2],c[3],c[3],c[2],c[2],s,c[2],c[0]);}%c";printf(s,c[0],c[2],c[3],c[3],c[3],c[2],c[3],c[3],c[2],c[2],s,c[2],c[0]);}
+int main(){
+/*
+"Yields falsehood when preceded by its quotation" yields falsehood when preceded by its quotation.
+*/
+char*c="\n\t\"\\";char*s="/*%c'What is there?' It can be answered, moreover, in a word - 'Everything' - and everyone will accept this answer as true.%c*/%c#include <stdio.h>%cint main(){%c/*%c%cYields falsehood when preceded by its quotation%c yields falsehood when preceded by its quotation.%c*/%cchar*c=%c%cn%ct%c%c%c%c%c;char*s=%c%s%c;printf(s,c[0],c[0],c[0],c[0],c[0],c[0],c[2],c[2],c[0],c[0],c[2],c[3],c[3],c[3],c[2],c[3],c[3],c[2],c[2],s,c[2],c[0]);}%c";printf(s,c[0],c[0],c[0],c[0],c[0],c[0],c[2],c[2],c[0],c[0],c[2],c[3],c[3],c[3],c[2],c[3],c[3],c[2],c[2],s,c[2],c[0]);}
