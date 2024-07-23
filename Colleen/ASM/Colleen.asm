526993076ebdb6f4030841025f83df8ac6f59893
fde-capu <flavio@carrara.us>
2024-07-22 21:25:06 -0300

    ... :( ...

--- Colleen/ASM/Colleen.asm
+++ Colleen/ASM/Colleen.asm
@@ -1,43 +1,33 @@
+; Comment A
 section .data
-msg db \
-	"section .data", 10, \
-	"msg db ", 92, 10, \
-	9, 34, "%s", 34, 10, \
-	"msg_len: equ $ - msg", 10, \
-	10, \
-	"section .text", 10, \
-	10, \
-	"global _start", 10, \
-	"_start:", 10, \
-	9, "call main", 10, \
-	9, "mov rax, 60", 10, \
-	9, "mov rdi, 0", 10, \
-	9, "syscall", 10, \
-	10, \
-	"main:", 10, \
-	9, "mov rax, 1", 10, \
-	9, "mov rdi, 1", 10, \
-	9, "lea rsi, [rel msg]", 10, \
-	9, "mov rdx, msg_len", 10, \
-	9, "syscall", 10, \
-	9, "ret", 10, 0
-msg_len: equ $ - msg
+s db "; Comment A%csection .data%cc db %c%cn%ct%c%c%c%c%c%cs...", 0
+f db 10,10,34,92,92,92,34,92,92,34,34
 
 section .text
+extern printf
 
 global _start
 _start:
+	lea rdi, [f]
+	lea rsi, [s]
+	mov ecx, 11
+	xor edx, edx
+
 	call main
 	mov rax, 60
 	mov rdi, 0
 	syscall
 
 main:
-	mov rax, 0
-	mov rdi, msg
-	mov rsi, msg
-	call printf
-	mov rax, 0
-	ret
+	; Comment B
+	dec ecx
+	cmp ecx, 0
+	je done
+	mov al, ','
+	stosb
+	inc byte [esi]
+	jmp main
 
-extern printf
+done:
+	call printf
+	ret
