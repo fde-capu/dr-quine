7c2164231743abdcdfa113ecd0a010d190015f52
fde-capu <flavio@carrara.us>
2024-07-27 23:04:09 -0300

    Working, now just check subject.

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,4 +1,4 @@
 //Comment!
 #include <stdio.h>
-#define X int main(){char*n="\n";char*q="\"";char*b="\\";const char*s="bla";FILE *fp=fopen("Grace_kid.c","w");fprintf(fp,"%s",s);fclose(fp);(void)n;(void)q;(void)b;}
+#define X int main(){char n=10;char q=34;char*s="//Comment!%c#include <stdio.h>%c#define X int main(){char n=10;char q=34;char*s=%c%s%c;FILE *fp=fopen(%cGrace_kid.c%c,%cw%c);fprintf(fp,s,n,n,q,s,q,q,q,q,q,n,n);fclose(fp);}%cX%c";FILE *fp=fopen("Grace_kid.c","w");fprintf(fp,s,n,n,q,s,q,q,q,q,q,n,n);fclose(fp);}
 X
