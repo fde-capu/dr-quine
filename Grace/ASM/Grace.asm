ac7eb50219e0109a0554dbe1a5c55bbd97ce9541
fde-capu <flavio@carrara.us>
2024-07-28 00:31:08 -0300

    Grace ASM is writing to file.

--- Grace/ASM/Grace.asm
+++ Grace/ASM/Grace.asm
@@ -1,9 +1,24 @@
+section .data
+o db 'Grace_kid.asm', 0
+m db 'w', 0
+s db '%s', 0
 section .text
-	extern fprintf
-	extern fopen
-	extern fclose
-	global _start
+extern fprintf
+extern fopen
+extern fclose
+global _start
 _start:
-	mov eax, 60
-	xor edi, edi
-	syscall
+mov rdi,o
+mov rsi,m
+call fopen ; now rax - FILE* to operate
+mov rdi,rax
+mov rsi,s
+mov rdx,o
+push rax
+xor rax,rax
+call fprintf
+pop rdi
+call fclose
+mov rax, 60
+xor rdi, rdi
+syscall
