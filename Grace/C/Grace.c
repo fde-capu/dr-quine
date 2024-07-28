83b844567f9a6cd4075e19bc3d81d019fe640731
fde-capu <flavio@carrara.us>
2024-07-27 21:39:17 -0300

    ...

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,4 +1,4 @@
 //Comment!
 #include <stdio.h>
-#define X int main(){char*c="\n\t\"\\";const char*s="bla";FILE *fp=fopen("Grace_kid.c","w");fprintf(fp,"%s",s);fclose(fp);(void)c;}
+#define X int main(){char*n="\n";char*q="\"";char*b="\\";const char*s="bla";FILE *fp=fopen("Grace_kid.c","w");fprintf(fp,"%s",s);fclose(fp);(void)n;(void)q;(void)b;}
 X
