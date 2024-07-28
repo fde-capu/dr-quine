54ccb3de0b1aa862e780a70c35ad8b031ac49a01
fde-capu <flavio@carrara.us>
2024-07-27 23:51:09 -0300

    Grace C not USES the extra (mandatory?) defines

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,6 +1,6 @@
-//Comment!
+//X
 #include <stdio.h>
-#define foo bar
-#define bar foo
-#define X int main(){char n=10;char q=34;char*s="//Comment!%c#include <stdio.h>%c#define foo bar%c#define bar foo%c#define X int main(){char n=10;char q=34;char*s=%c%s%c;FILE *fp=fopen(%cGrace_kid.c%c,%cw%c);fprintf(fp,s,n,n,n,n,q,s,q,q,q,q,q,n,n);fclose(fp);}%cX%c";FILE *fp=fopen("Grace_kid.c","w");fprintf(fp,s,n,n,n,n,q,s,q,q,q,q,q,n,n);fclose(fp);}
+#define N 10
+#define Q 34
+#define X int main(){char n=N;char q=Q;char*s="//X%c#include <stdio.h>%c#define N 10%c#define Q 34%c#define X int main(){char n=N;char q=Q;char*s=%c%s%c;FILE *fp=fopen(%cGrace_kid.c%c,%cw%c);fprintf(fp,s,n,n,n,n,q,s,q,q,q,q,q,n,n);fclose(fp);}%cX%c";FILE *fp=fopen("Grace_kid.c","w");fprintf(fp,s,n,n,n,n,q,s,q,q,q,q,q,n,n);fclose(fp);}
 X
