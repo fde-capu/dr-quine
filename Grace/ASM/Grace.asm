69c74ca97fcd89a977478e5c109641f49c0f7940
fde-capu <flavio@carrara.us>
2024-07-28 04:22:16 -0300

    Only four arguments?

--- Grace/ASM/Grace.asm
+++ Grace/ASM/Grace.asm
@@ -1,14 +1,21 @@
 ;X
 %define N 10
-%define Q 34
-%define F 'Grace_kit.asm'
+%define Q 39
+%define P 37 ;;; X
+%macro R 2
+%rep %2
+push %1
+%endrep
+%endmacro
 %macro X 0
 section .data
-o db F, 0
-m db 'w', 0
-n db N
-q db Q
-s db '%s', 0
+o db 'Grace_kid.asm',0
+m db 'w',0
+n db N,0
+q db Q,0
+p db P,0
+pn db P,N,0
+s db ';X--%s%s%s%s%s--',0
 section .text
 extern fprintf
 extern fopen
@@ -19,15 +26,18 @@ mov rdi,o
 mov rsi,m
 call fopen
 mov rdi,rax
-mov rsi,s
-mov rdx,o
 push rax
+mov rsi,s
+mov rdx,q
+mov rcx,q
+mov r8,q
+mov r9,q
 xor rax,rax
 call fprintf
 pop rdi
 call fclose
-mov rax, 60
-xor rdi, rdi
+mov rax,60
+xor rdi,rdi
 syscall
 %endmacro
 X
