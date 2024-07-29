00febbe3645f3bf43886a8af9d68d550c36244b4
fde-capu <flavio@carrara.us>
2024-07-28 22:23:04 -0300

    Grace C with multiline comments.

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,6 +1,8 @@
-//X
+/*
+X
+*/
 #include <stdio.h>
 #define N 10
 #define Q 34
-#define X int main(){char n=N;char q=Q;char*s="//X%c#include <stdio.h>%c#define N 10%c#define Q 34%c#define X int main(){char n=N;char q=Q;char*s=%c%s%c;FILE *f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,s,n,n,n,n,q,s,q,q,q,q,q,n,n);fclose(f);}%cX%c";FILE *f=fopen("Grace_kid.c","w");fprintf(f,s,n,n,n,n,q,s,q,q,q,q,q,n,n);fclose(f);}
+#define X int main(){char n=N;char q=Q;char*s="/*%cX%c*/%c#include <stdio.h>%c#define N 10%c#define Q 34%c#define X int main(){char n=N;char q=Q;char*s=%c%s%c;FILE *f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,s,n,n,n,n,n,n,q,s,q,q,q,q,q,n,n);fclose(f);}%cX%c";FILE *f=fopen("Grace_kid.c","w");fprintf(f,s,n,n,n,n,n,n,q,s,q,q,q,q,q,n,n);fclose(f);}
 X
