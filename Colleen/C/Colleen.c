39fd2222417d2a96ec1e74a2b398add75f9b4398
fde-capu <flavio@carrara.us>
2024-07-28 22:11:51 -0300

    Re-arranged comments.

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,6 +1,10 @@
-//X
+/*
+X
+*/
 #include <stdio.h>
 char get_n(){return 10;}
 int main(){
-//X
-char n=get_n();char q=34;char*s="//X%c#include <stdio.h>%cchar get_n(){return 10;}%cint main(){%c//X%cchar n=get_n();char q=34;char*s=%c%s%c;printf(s,n,n,n,n,n,q,s,q,n);}%c";printf(s,n,n,n,n,n,q,s,q,n);}
+/*
+X
+*/
+char n=get_n();char q=34;char*s="/*%cX%c*/%c#include <stdio.h>%cchar get_n(){return 10;}%cint main(){%c/*%cX%c*/%cchar n=get_n();char q=34;char*s=%c%s%c;printf(s,n,n,n,n,n,n,n,n,n,q,s,q,n);}%c";printf(s,n,n,n,n,n,n,n,n,n,q,s,q,n);}
