d5e5a700bab81b4094422140ccd66312f0901662
fde-capu <flavio@carrara.us>
2024-07-28 21:47:50 -0300

    Fixed (it was working but was wrong...)

--- Grace/ASM/Grace.asm
+++ Grace/ASM/Grace.asm
@@ -15,7 +15,7 @@ n db N,0
 q db Q,0
 p db P,0
 np db N,P,0
-s db ';X%s%sdefine N 10%s%sdefine Q 39%sdefine P 37%smacro D 3%srep %s3%s1 %s2%sendrep%sendmacro%smacro X 0%ssection .data%so db %sGrace_kid.asm%s,0%sm db %sw%s,0%sn db N,0%sq db Q,0%sp db P,0%snp db N,P,0%ss db %s%s%s,0%ssection .text%sextern fprintf%sextern fopen%sextern fclose%sglobal _start%s_start:%smov rdi,o%smov rsi,m%scall fopen%smov rdi,rax%smov rsi,s%smov rdx,n%smov rcx,p%smov r8,n%smov r9,p%spush rax%sD push,n,2%spush p%sD push,n,3%spush np%sD push,n,41%spush q%spush s%spush q%sD push,n,5%sD push,q,2%spush n%sD push,q,2%sD push,n,2%sD push,np,3%spush p%spush np%spush p%sD push,np,3%sxor rax,rax%scall fprintf%sD pop,rdi,73%scall fclose%smov rax,60%sxor rdi,rdi%ssyscall%sendmacro%sX%s',0
+s db ';X%s%sdefine N 10%s%sdefine Q 39%sdefine P 37%smacro D 3%srep %s3%s1 %s2%sendrep%sendmacro%smacro X 0%ssection .data%so db %sGrace_kid.asm%s,0%sm db %sw%s,0%sn db N,0%sq db Q,0%sp db P,0%snp db N,P,0%ss db %s%s%s,0%ssection .text%sextern fprintf%sextern fopen%sextern fclose%sglobal _start%s_start:%smov rdi,o%smov rsi,m%scall fopen%smov rdi,rax%smov rsi,s%smov rdx,n%smov rcx,p%smov r8,n%smov r9,p%spush rax%sD push,n,2%spush np%sD push,n,39%spush q%spush s%spush q%sD push,n,5%sD push,q,2%spush n%sD push,q,2%sD push,n,2%sD push,np,3%spush p%spush np%spush p%sD push,np,3%sxor rax,rax%scall fprintf%sD pop,rdi,67%scall fclose%smov rax,60%sxor rdi,rdi%ssyscall%sendmacro%sX%s',0
 section .text
 extern fprintf
 extern fopen
@@ -33,10 +33,8 @@ mov r8,n
 mov r9,p
 push rax
 D push,n,2
-push p
-D push,n,3
 push np
-D push,n,41
+D push,n,39
 push q
 push s
 push q
@@ -52,7 +50,7 @@ push p
 D push,np,3
 xor rax,rax
 call fprintf
-D pop,rdi,73
+D pop,rdi,67
 call fclose
 mov rax,60
 xor rdi,rdi
