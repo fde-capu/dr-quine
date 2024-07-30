1c28d57a1927a054a6ea7e45f99238725fab1081
fde-capu <flavio@carrara.us>
2024-07-30 01:26:33 -0300

    Self reproduction working. Now set up the proper quine, then run each kid on system(3).

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -1,4 +1,3 @@
-;X
 %define N 10
 %define Q 39
 %define P 37
@@ -16,31 +15,34 @@ q db Q,0
 p db P,0
 np db N,P,0
 
-nasm db 'nasm -f elf64 -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2 Sully_%d | ld -o Sully_%d && ./Sully_%d',0
+nasm db 'nasm -f elf64 -o Sully_%d.o Sully_%d.s && ld -o Sully_%d Sully_%d.o -m elf_x86_64 -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2',0
 
-s db ';X%s%sdefine N 10%s%sdefine Q 39%sdefine P 37%smacro D 3%srep %s3%s1 %s2%sendrep%sendmacro%smacro X 0%ssection .data%so db %sSully_4.s%s,0%sm db %sw%s,0%sn db N,0%sq db Q,0%sp db P,0%snp db N,P,0%ss db %s%s%s,0%ssection .text%sextern fprintf%sextern fopen%sextern fclose%sglobal _start%s_start:%smov r10,5%scmp r10,1%sjl _out%ssub r10,1%smov rdi,o%smov rsi,m%scall fopen%smov rdi,rax%smov rsi,s%smov rdx,n%smov rcx,p%smov r8,n%smov r9,p%spush rax%sD push,n,2%spush np%sD push,n,44%spush q%spush s%spush q%sD push,n,5%sD push,q,2%spush n%sD push,q,2%sD push,n,2%sD push,np,3%spush p%spush np%spush p%sD push,np,3%sxor rax,rax%scall fprintf%sD pop,rdi,72%scall fclose%s_out:%smov rax,60%sxor rdi,rdi%ssyscall%sendmacro%sX%s',0
+s db ';X%s%sdefine N 10%s%sdefine Q 39%sdefine P 37%smacro D 3%srep %s3%s1 %s2%sendrep%sendmacro%smacro X 0%ssection .data%so db %sSully_%sd.s%s,0%sm db %sw%s,0%sn db N,0%sq db Q,0%sp db P,0%snp db N,P,0%ss db %s%s%s,0%ssection .text%sextern fprintf%sextern fopen%sextern fclose%sglobal _start%s_start:%smov r12,5%scmp r12,1%sjl _out%ssub r12,1%smov rdi,o%smov rsi,m%scall fopen%smov rdi,rax%smov rsi,s%smov rdx,n%smov rcx,p%smov r8,n%smov r9,p%spush rax%sD push,n,2%spush np%sD push,n,44%spush q%spush s%spush q%sD push,n,5%sD push,q,2%spush n%sD push,q,2%sD push,n,2%sD push,np,3%spush p%spush np%spush p%sD push,np,3%sxor rax,rax%scall fprintf%sD pop,rdi,72%scall fclose%s_out:%smov rax,60%sxor rdi,rdi%ssyscall%sendmacro%sX%s',0
 section .text
 extern fprintf
 extern sprintf
 extern fopen
 extern fclose
+extern system
 global _start
 _start:
-mov r10,5
-cmp r10,1
+mov r12,5
+cmp r12,1
 jl _out
-sub r10,1
+sub r12,1
 
 mov rdi,rsp
 mov rsi,o
-mov rdx,r10
+mov rdx,r12
 xor rax,rax
 call sprintf
 
 mov rdi,rsp
 mov rsi,m
 call fopen
-mov rdi,rax
+mov rbx,rax
+
+mov rdi,rbx
 mov rsi,s
 mov rdx,n
 mov rcx,p
@@ -56,7 +58,9 @@ push q
 D push,n,5
 D push,q,2
 push n
-D push,q,2
+push q
+push p
+push q
 D push,n,2
 D push,np,3
 push p
@@ -65,8 +69,23 @@ push p
 D push,np,3
 xor rax,rax
 call fprintf
-D pop,rdi,72
+
+mov rdi,rbx
 call fclose
+
+mov rdi,rsp
+mov rsi,nasm
+mov rdx,r12
+mov rcx,r12
+mov r8,r12
+mov r9,r12
+xor rax,rax
+call sprintf
+
+mov rdi,rsp
+push rsp
+call system
+
 _out:
 mov rax,60
 xor rdi,rdi
