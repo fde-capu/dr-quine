e8ddfa2cf6acbebaf1c1af4604a9694e75d67d2e
fde-capu <flavio@carrara.us>
2024-08-05 19:28:32 -0300

    Grace.s remake.

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -4,11 +4,6 @@
 %define N 10
 %define Q 39
 %define P 37
-%macro D 3
-%rep %3
-%1 %2
-%endrep
-%endmacro
 %macro X 0
 section .data
 o db 'Grace_kid.s',0
@@ -16,8 +11,7 @@ m db 'w',0
 n db N,0
 q db Q,0
 p db P,0
-np db N,P,0
-s db ';X%s%sdefine N 10%s%sdefine Q 39%sdefine P 37%smacro D 3%srep %s3%s1 %s2%sendrep%sendmacro%smacro X 0%ssection .data%so db %sGrace_kid.s%s,0%sm db %sw%s,0%sn db N,0%sq db Q,0%sp db P,0%snp db N,P,0%ss db %s%s%s,0%ssection .text%sextern fprintf%sextern fopen%sextern fclose%sglobal _start%s_start:%smov rdi,o%smov rsi,m%scall fopen%smov rdi,rax%smov rsi,s%smov rdx,n%smov rcx,p%smov r8,n%smov r9,p%spush rax%sD push,n,2%spush np%sD push,n,39%spush q%spush s%spush q%sD push,n,5%sD push,q,2%spush n%sD push,q,2%sD push,n,2%sD push,np,3%spush p%spush np%spush p%sD push,np,3%sxor rax,rax%scall fprintf%sD pop,rdi,67%scall fclose%smov rax,60%sxor rdi,rdi%ssyscall%sendmacro%sX%s',0
+s db ';%1$s; X%1$s;%1$s%3$sdefine N 10%1$s%3$sdefine Q 39%1$s%3$sdefine P 37%1$s%3$smacro X 0%1$ssection .data%1$so db %2$sGrace_kid.s%2$s,0%1$sm db %2$sw%2$s,0%1$sn db N,0%1$sq db Q,0%1$sp db P,0%1$ss db %2$s%4$s%2$s,0%1$ssection .text%1$sextern fprintf%1$sextern fopen%1$sextern fclose%1$sglobal _start%1$s_start:%1$smov rdi,o%1$smov rsi,m%1$scall fopen%1$smov rbx,rax%1$smov rdi,rbx%1$smov rsi,s%1$smov rdx,n%1$smov rcx,q%1$smov r8,p%1$smov r9,s%1$sxor rax,rax%1$scall fprintf%1$smov rdi,rbx%1$sxor rax,rax%1$scall fclose%1$smov rax,60%1$sxor rdi,rdi%1$ssyscall%1$s%3$sendmacro%1$sX%1$s',0
 section .text
 extern fprintf
 extern fopen
@@ -27,7 +21,8 @@ _start:
 mov rdi,o
 mov rsi,m
 call fopen
-mov rdi,rax
+mov rbx,rax
+mov rdi,rbx
 mov rsi,s
 mov rdx,n
 mov rcx,q
@@ -35,7 +30,8 @@ mov r8,p
 mov r9,s
 xor rax,rax
 call fprintf
-D pop,rdi,67
+mov rdi,rbx
+xor rax,rax
 call fclose
 mov rax,60
 xor rdi,rdi
