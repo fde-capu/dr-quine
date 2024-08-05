745dedb65313febeeee1b62fe80b0b72abef57b4
fde-capu <flavio@carrara.us>
2024-08-05 16:52:46 -0300

    Semmingly working perfect.

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -5,11 +5,9 @@ n db 10
 q db 39
 p db 37
 
-nasm db 'nasm -f elf64 -o Sully_%d.o Sully_%d.s',0
-ld db 'ld -o Sully_%d Sully_%d.o -m elf_x86_64 -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2',0
-run db './Sully_%d',0
+nasm db 'nasm -f elf64 -o Sully_%1$d.o Sully_%1$d.s && ld -o Sully_%1$d Sully_%1$d.o -m elf_x86_64 -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2 && ./Sully_%1$d',0
 
-s db 'section .data%1$co db %2$cSully_%3$cd.s%2$c,0%1$cm db %2$cw%2$c,0%1$cn db 10%1$cq db 39%1$cp db 37%1$c%1$cnasm db %2$cnasm -f elf64 -o Sully_%3$cd.o Sully_%3$cd.s%2$c,0%1$cld db %2$cld -o Sully_%3$cd Sully_%3$cd.o -m elf_x86_64 -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2%2$c,0%1$crun db %2$c./Sully_%3$cd%2$c,0%1$c%1$cs db %2$c%5$s%2$c,0%1$c%1$csection .text%1$cextern fprintf%1$cextern sprintf%1$cextern fopen%1$cextern fclose%1$cextern system%1$cglobal _start%1$c%1$c_start:%1$cmov r12,$5%d%1$ccmp r12,1%1$cjl _out%1$csub r12,1%1$c%1$c_out_file:%1$cmov rdi,rsp%1$cmov rsi,o%1$cmov rdx,r12%1$cxor rax,rax%1$ccall sprintf%1$cmov rdi,rsp%1$cmov rsi,m%1$ccall fopen%1$cmov rbx,rax%1$c%1$c_format_s:%1$cmov rdi,rax%1$cmov rsi,s%1$cmov rdx,[n]%1$cmov rcx,[q]%1$cmov r8,[p]%1$cmov r9,s%1$cpush r12%1$cxor rax,rax%1$ccall fprintf%1$cpop r12%1$c%1$cmov rdi,rbx%1$ccall fclose%1$c%1$c_format_nasm_str:%1$clea rdi,[rsp]%1$cmov rsi,nasm%1$cmov rdx,r12%1$cmov rcx,r12%1$cmov r8,r12%1$cmov r9,r12%1$cxor rax,rax%1$ccall sprintf%1$c%1$c_compile:%1$cmov rdi,rsi%1$ccall system%1$c%1$c_out:%1$cmov rax,60%1$cxor rdi,rdi%1$csyscall%1$c',0
+s db 'section .data%1$co db %2$cSully_%3$cd.s%2$c,0%1$cm db %2$cw%2$c,0%1$cn db 10%1$cq db 39%1$cp db 37%1$c%1$cnasm db %2$cnasm -f elf64 -o Sully_%3$c1$d.o Sully_%3$c1$d.s && ld -o Sully_%3$c1$d Sully_%3$c1$d.o -m elf_x86_64 -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2 && ./Sully_%3$c1$d%2$c,0%1$c%1$cs db %2$c%4$s%2$c,0%1$c%1$csection .text%1$cextern fprintf%1$cextern sprintf%1$cextern fopen%1$cextern fclose%1$cextern system%1$cglobal _start%1$c%1$c_start:%1$cmov r12,%5$d%1$ccmp r12,1%1$cjl _out%1$csub r12,1%1$c%1$c_out_file:%1$cmov rdi,rsp%1$cmov rsi,o%1$cmov rdx,r12%1$cxor rax,rax%1$ccall sprintf%1$cmov rdi,rsp%1$cmov rsi,m%1$ccall fopen%1$cmov rbx,rax%1$c%1$c_format_s:%1$cmov rdi,rax%1$cmov rsi,s%1$cmov rdx,[n]%1$cmov rcx,[q]%1$cmov r8,[p]%1$cmov r9,s%1$cpush r12%1$cxor rax,rax%1$ccall fprintf%1$cpop r12%1$cmov rdi,rbx%1$ccall fclose%1$c%1$c_format_nasm_str:%1$csub rsp,200%1$cmov rdi,rsp%1$cmov rsi,nasm%1$cmov rdx,r12%1$cxor rax,rax%1$ccall sprintf%1$c%1$c_compile:%1$cmov rdi,rsp%1$cpush rsp%1$ccall system%1$c%1$c_out:%1$cmov rax,60%1$cxor rdi,rdi%1$csyscall%1$c',0
 
 section .text
 extern fprintf
@@ -47,22 +45,20 @@ push r12
 xor rax,rax
 call fprintf
 pop r12
-
 mov rdi,rbx
 call fclose
 
 _format_nasm_str:
-lea rdi,[rsp]
+sub rsp,200
+mov rdi,rsp
 mov rsi,nasm
 mov rdx,r12
-mov rcx,r12
-mov r8,r12
-mov r9,r12
 xor rax,rax
 call sprintf
 
 _compile:
-mov rdi,rsi
+mov rdi,rsp
+push rsp
 call system
 
 _out:
--- Sully/ASM/vim.vim
+++ Sully/ASM/vim.vim
@@ -5,6 +5,6 @@ function! QUINE()
 	execute '%s/_N_/%1$c/g'
 	execute '%s/_Q_/%2$c/g'
 	execute '%s/_P_/%3$c/g'
-	execute '%s/mov r12,5/mov r12,$5%d/g'
-execute 's/s db %2$c%2$c/s db %2$c%5$s%2$c/'
+	execute 's/s db %2$c%2$c/s db %2$c%4$s%2$c/'
+	execute '%s/mov r12,5/mov r12,%5$d/g'
 endfunction
