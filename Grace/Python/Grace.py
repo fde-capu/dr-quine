f2883769e701497af7efae79ac31f98db4428f2f
fde-capu <flavio@carrara.us>
2024-08-06 16:07:57 -0300

    Grace.py better yet, calling FT(x).

--- Grace/Python/Grace.py
+++ Grace/Python/Grace.py
@@ -6,12 +6,12 @@ import os
 n = chr(0x0A)
 q = chr(0x27)
 t = chr(0x09)
-FT='''
-def main():
-	s = '#!/bin/python3{n}#{n}# X{n}#{n}import os{n}n = chr(0x0A){n}q = chr(0x27){n}t = chr(0x09){n}FT={q}{q}{q}{n}def main():{n}{t}s = {q}S{q}{n}{t}with open({q}Grace_kid.py{q}, {q}w{q}) as file:{n}{t}{t}file.write(s.format(n = n, q = q, t = t).replace({q}S{q}, s, 1)){n}main(){n}{q}{q}{q}{n}os.system({q}chmod +x Grace_kid.py{q}){n}exec(FT){n}'
+s='''
+def FT(x):
+	s = '#!/bin/python3{n}#{n}# X{n}#{n}import os{n}n = chr(0x0A){n}q = chr(0x27){n}t = chr(0x09){n}s={q}{q}{q}{n}def FT(x):{n}{t}s = {q}S{q}{n}{t}with open({q}Grace_kid.py{q}, {q}w{q}) as file:{n}{t}{t}file.write(s.format(n = n, q = q, t = t).replace({q}S{q}, s, 1)){n}{t}os.system({q}chmod +x Grace_kid.py{q}){n}{q}{q}{q}{n}exec(s){n}FT(s){n}'
 	with open('Grace_kid.py', 'w') as file:
 		file.write(s.format(n = n, q = q, t = t).replace('S', s, 1))
-main()
+	os.system('chmod +x Grace_kid.py')
 '''
-os.system('chmod +x Grace_kid.py')
-exec(FT)
+exec(s)
+FT(s)
