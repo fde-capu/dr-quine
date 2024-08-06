192420983a968a74202969677728c62ebc1fbbf5
fde-capu <flavio@carrara.us>
2024-08-05 23:19:49 -0300

    Colleen.py now using secondary function.

--- Colleen/Python/Colleen.py
+++ Colleen/Python/Colleen.py
@@ -5,11 +5,13 @@
 n = chr(0x0A)
 q = chr(0x27)
 t = chr(0x09)
-s = '#!/bin/python3{n}#{n}# X{n}#{n}n = chr(0x0A){n}q = chr(0x27){n}t = chr(0x09){n}s = {q}SS{q}{n}def main():{n}#{n}# X{n}#{n}{t}print(s.format(n = n, q = q, t = t).replace({q}SS{q}, s, 1)){n}{n}main();'
+s = '#!/bin/python3{n}#{n}# X{n}#{n}n = chr(0x0A){n}q = chr(0x27){n}t = chr(0x09){n}s = {q}S{q}{n}def return1():{n}{t}return 1{n}def main():{n}#{n}# X{n}#{n}{t}print(s.format(n = n, q = q, t = t).replace({q}S{q}, s, return1())){n}{n}main();'
+def return1():
+	return 1
 def main():
 #
 # X
 #
-	print(s.format(n = n, q = q, t = t).replace('SS', s, 1))
+	print(s.format(n = n, q = q, t = t).replace('S', s, return1()))
 
 main();
--- test_all.sh
+++ test_all.sh
@@ -1,28 +1,53 @@
 #!/bin/sh
 echo
-echo -n "=== Colleen/C =====> "
+echo -n "=== Colleen/C ========> "
 cd Colleen/C
 make d
 make fclean
-echo -n "=== Colleen/ASM ===> "
-cd ../../Colleen/ASM
-make d
-make fclean
-echo -n "=== Grace/C =======> "
+
+echo -n "=== Grace/C ==========> "
 cd ../../Grace/C
 make d
 make fclean
-echo -n "=== Grace/ASM =====> "
-cd ../../Grace/ASM
-make d
-make fclean
-echo -n "=== Sully/C =======> "
+
+echo -n "=== Sully/C ==========> "
 cd ../../Sully/C
 make d
 make fclean
-echo -n "=== Sully/ASM =====> "
+
+echo -n "=== Colleen/ASM ======> "
+cd ../../Colleen/ASM
+make d
+make fclean
+
+echo -n "=== Grace/ASM ========> "
+cd ../../Grace/ASM
+make d
+make fclean
+
+echo -n "=== Sully/ASM ========> "
 cd ../../Sully/ASM
 make d
 make fclean
+
+
+echo -n "=== Colleen/Python ===> "
+cd ../../Colleen/Python
+./Colleen.py > b
+diff Colleen.py b > /dev/null && echo '[ OK ]' || echo '[ KO ]'
+rm b
+
+echo -n "=== Grace/Python =====> "
+cd ../../Grace/Python
+./Grace.py > b
+diff Grace.py b > /dev/null && echo '[ OK ]' || echo '[ KO ]'
+rm b
+
+echo -n "=== Sully/Python =====> "
+cd ../../Sully/Python
+./Sully.py > b
+diff Sully.py b > /dev/null && echo '[ OK ]' || echo '[ KO ]'
+rm b
+
 cd ../..
 echo
